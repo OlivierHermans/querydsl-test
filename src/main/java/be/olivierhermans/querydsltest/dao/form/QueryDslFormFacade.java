75896cc527b77ab8c1845769da013aaa809b0f33
Olivier Hermans <oli4hermans@gmail.com>
2021-07-18 11:10:59 +0200

    Implement paging

--- src/main/java/be/olivierhermans/querydsltest/dao/form/QueryDslFormFacade.java
+++ src/main/java/be/olivierhermans/querydsltest/dao/form/QueryDslFormFacade.java
@@ -3,69 +3,82 @@ package be.olivierhermans.querydsltest.dao.form;
 import be.olivierhermans.querydsltest.dao.SForm;
 import be.olivierhermans.querydsltest.model.Form;
 import be.olivierhermans.querydsltest.service.form.port.out.FormFacade;
+import com.querydsl.core.FetchableQuery;
 import com.querydsl.core.Tuple;
-import com.querydsl.core.support.FetchableQueryBase;
+import com.querydsl.core.support.QueryBase;
 import com.querydsl.core.types.Expression;
 import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.querydsl.jpa.sql.JPASQLQuery;
-import com.querydsl.sql.*;
+import com.querydsl.sql.PostgreSQLTemplates;
+import com.querydsl.sql.SQLExpressions;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
-import javax.sql.DataSource;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.util.Collection;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
 import static java.util.stream.Collectors.toList;
 
+@RequiredArgsConstructor
 @Service
 @Primary
 class QueryDslFormFacade implements FormFacade {
 
-    private final JPAQueryFactory jpaQueryFactory;
-    private final SQLQueryFactory sqlQueryFactory;
     private final EntityManager entityManager;
     private final FormEntityMapper mapper;
-
-    public QueryDslFormFacade(EntityManager entityManager, DataSource dataSource, FormEntityMapper mapper) {
-        this.entityManager = entityManager;
-        this.jpaQueryFactory = new JPAQueryFactory(entityManager);
-        this.sqlQueryFactory = new SQLQueryFactory(new Configuration(SQLTemplates.DEFAULT), dataSource);
-        this.mapper = mapper;
-    }
 
     @Override
     public Collection<Form> findAll() {
         final SForm form = SForm.form;
-        final QFormEntity formEntity = QFormEntity.formEntity;
         final Expression<Long> rowNumber = SQLExpressions.rowNumber()
                 .over()
                 .partitionBy(form.clientId)
                 .orderBy(form.creationTms.desc()).as("rowNumber");
-        final JPASQLQuery query = new JPASQLQuery(entityManager, new PostgreSQLTemplates());
-
-        final FetchableQueryBase queryBase =
-                (FetchableQueryBase) query.select(form.formId, form.clientId, form.creationTms)
+        final Supplier<QueryBase> querySupplier = () ->
+                new JPASQLQuery(entityManager, new PostgreSQLTemplates())
+                        .select(form.formId, form.clientId, form.creationTms)
                         .from(SQLExpressions.select(form.formId, form.clientId, form.creationTms, rowNumber)
                                 .from(form).as(form))
-                        .where(Expressions.numberPath(Long.class, "rowNumber").eq(1L)).where(form.clientId.eq(2L));
-        final Collection<Tuple> tuples = (Collection<Tuple>) queryBase.fetch();
-        final Collection<FormEntity> entities = tuples.stream().map(tuple -> {
-            final FormEntity entity = new FormEntity();
-            entity.setFormId(tuple.get(form.formId));
-            entity.setClientId(tuple.get(form.clientId));
-            entity.setCreationTms(timestampToOffsetDateTime(tuple.get(form.creationTms)));
-            return entity;
-        }).collect(toList());
-        return entities.stream().map(mapper::entityToModel).collect(toList());
+                        .where(Expressions.numberPath(Long.class, "rowNumber").eq(1L));
+        final Long numberOfRecords = fetchCount(querySupplier);
+        final long pageSize = 1;
+
+        return LongStream.rangeClosed(5, 8/*numberOfRecords / pageSize*/)   // test with non existing pages
+                .mapToObj(pageNum ->
+                        fetch(querySupplier, pageNum, pageSize).stream()
+                                .map(this::mapFormTuple)
+                                .map(mapper::entityToModel)
+                                .collect(toList()))
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
+    }
+
+    private Long fetchCount(Supplier<QueryBase> querySupplier) {
+        return ((FetchableQuery) querySupplier.get()).fetchCount();
+    }
+
+    private Collection<Tuple> fetch(Supplier<QueryBase> querySupplier, long pageNum, long pageSize) {
+        return (Collection<Tuple>) ((FetchableQuery) querySupplier.get().limit(pageSize).offset((pageNum - 1) * pageSize)).fetch();
+    }
+
+    private FormEntity mapFormTuple(Tuple tuple) {
+        final FormEntity entity = new FormEntity();
+        entity.setFormId(tuple.get(SForm.form.formId));
+        entity.setClientId(tuple.get(SForm.form.clientId));
+        entity.setCreationTms(timestampToOffsetDateTime(tuple.get(SForm.form.creationTms)));
+        return entity;
     }
 
     private OffsetDateTime timestampToOffsetDateTime(Timestamp timestamp) {
-        return OffsetDateTime.ofInstant(Instant.ofEpochMilli(timestamp.getTime()), ZoneOffset.UTC);
+        return OffsetDateTime.ofInstant(Instant.ofEpochMilli(timestamp.getTime()), ZoneOffset.ofHours(2));
     }
 }
